e6da13b7f8884c3be7d97735fa50b8f8e91799b3
Kyeongtaek Lee <skylove0911@naver.com>
2025-05-17 18:33:35 +0900

    [feat] 주석 해제

--- RandEngine/RandEngine/Engine/Source/Runtime/Engine/Classes/Particle/ParticleHelper.h
+++ RandEngine/RandEngine/Engine/Source/Runtime/Engine/Classes/Particle/ParticleHelper.h
@@ -173,17 +173,17 @@ struct FAsyncBufferFillData
     /** View for this buffer fill task   */
     const FEditorViewportClient* View;
     /** Number of verts in VertexData   */
-    int32									VertexCount;
+    int32 VertexCount;
     /** Stride of verts, used only for error checking   */
-    int32									VertexSize;
+    int32 VertexSize;
     /** Pointer to vertex data   */
     void* VertexData;
     /** Number of indices in IndexData   */
-    int32									IndexCount;
+    int32 IndexCount;
     /** Pointer to index data   */
     void* IndexData;
     /** Number of triangles filled in   */
-    int32									OutTriangleCount;
+    int32 OutTriangleCount;
     /** Pointer to dynamic parameter data */
     void* DynamicParameterData;
 
@@ -440,17 +440,6 @@ struct FDynamicSpriteEmitterDataBase : public FDynamicEmitterDataBase
     {
     }
 
-
-    /**
-     *	Debug rendering
-     *
-     *	@param	Proxy		The primitive scene proxy for the emitter.
-     *	@param	PDI			The primitive draw interface to render with
-     *	@param	View		The scene view being rendered
-     *	@param	bCrosses	If true, render Crosses at particle position; false, render points
-     */
-    //virtual void RenderDebug(const FParticleSystemSceneProxy* Proxy, FPrimitiveDrawInterface* PDI, const FSceneView* View, bool bCrosses) const;
-
     /**
      *	Set up an buffer for async filling
      *
@@ -460,8 +449,7 @@ struct FDynamicSpriteEmitterDataBase : public FDynamicEmitterDataBase
      *	@param	InVertexSize			Stride of these verts, only used for verification
      *	@param	InDynamicParameterVertexStride	Stride of the dynamic parameter
      */
-    /*void BuildViewFillData(
-        const FParticleSystemSceneProxy* Proxy,
+    void BuildViewFillData(
         const FEditorViewportClient* InView,
         int32 InVertexCount,
         int32 InVertexSize,
@@ -471,7 +459,7 @@ struct FDynamicSpriteEmitterDataBase : public FDynamicEmitterDataBase
         FGlobalDynamicVertexBufferAllocation& DynamicVertexAllocation,
         FGlobalDynamicIndexBufferAllocation& DynamicIndexAllocation,
         FGlobalDynamicVertexBufferAllocation* DynamicParameterAllocation,
-        FAsyncBufferFillData& Data) const;*/
+        FAsyncBufferFillData& Data) const;
 
     const UMaterial* MaterialResource;
 
