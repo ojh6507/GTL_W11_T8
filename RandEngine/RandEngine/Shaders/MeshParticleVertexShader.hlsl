045e7a33e5aeffa38f887e271ab53591edd43ed3
허승빈 <qmfhfl02@gmail.com>
2025-05-20 00:45:36 +0900

    Mesh Particle Vertex Shader

--- RandEngine/RandEngine/Shaders/MeshParticleVertexShader.hlsl
+++ RandEngine/RandEngine/Shaders/MeshParticleVertexShader.hlsl
@@ -1,18 +1,73 @@
 #include "ShaderRegisters.hlsl"
 
-struct VS_InstanceData   
+#ifdef LIGHTING_MODEL_GOURAUD
+SamplerState DiffuseSampler : register(s0);
+
+Texture2D DiffuseTexture : register(t0);
+
+cbuffer MaterialConstants : register(b1)
 {
-    float4 Color : COLOR;
-    float4 Transform0 : TEXCOORD0;
-    float4 Transform1 : TEXCOORD1;
-    float4 Transform2 : TEXCOORD2;
-    float4 Velocity : TEXCOORD3; 
-    int4 SubUVParams : TEXCOORD4;
-    float SubUVLerp : TEXCOORD5;
-    float RelativeTime : TEXCOORD6;
+    FMaterial Material;
+}
+
+#include "Light.hlsl"
+#endif
+
+struct VS_Input  
+{
+    float3 Position : POSITION;
+    float4 MeshColor : COLOR;
+    float3 Normal : NORMAL;
+    float4 Tangent : TANGENT;
+    float2 UV : TEXCOORD;
+    uint MaterialIndex : MATERIAL_INDEX;
+    
+    float4 Color : COLOR1;
+    float4 Transform0 : TEXCOORD1;
+    float4 Transform1 : TEXCOORD2;
+    float4 Transform2 : TEXCOORD3;
+    float4 Velocity : TEXCOORD4; 
+    int4 SubUVParams : TEXCOORD5;
+    float SubUVLerp : TEXCOORD6;
+    float RelativeTime : TEXCOORD7;
 };
 
-float4 mainVS(float4 pos : POSITION) : SV_POSITION
+PS_INPUT_StaticMesh mainVS(VS_Input Input)
 {
-    return pos;
+    PS_INPUT_StaticMesh Output;
+
+    Output.Position = float4(Input.Position, 1.0);
+    float4x4 Transfrom = (Input.Transform0, Input.Transform1, Input.Transform2, float4(0, 0, 0, 1));
+    Output.Position = mul(Output.Position, Transfrom);
+    Output.WorldPosition = Output.Position.xyz;
+    
+    Output.Position = mul(Output.Position, ViewMatrix);
+    Output.Position = mul(Output.Position, ProjectionMatrix);
+    
+    Output.WorldNormal = mul(Input.Normal, (float3x3) InverseTransposedWorld);
+
+    // Begin Tangent
+    float3 WorldTangent = mul(Input.Tangent.xyz, (float3x3) WorldMatrix);
+    WorldTangent = normalize(WorldTangent);
+    WorldTangent = normalize(WorldTangent - Output.WorldNormal * dot(Output.WorldNormal, WorldTangent));
+
+    Output.WorldTangent = float4(WorldTangent, Input.Tangent.w);
+    // End Tangent
+    
+    Output.UV = Input.UV;
+    Output.MaterialIndex = Input.MaterialIndex;
+
+#ifdef LIGHTING_MODEL_GOURAUD
+    float3 DiffuseColor = Input.Color;
+    if (Material.TextureFlag & TEXTURE_FLAG_DIFFUSE)
+    {
+        DiffuseColor = DiffuseTexture.SampleLevel(DiffuseSampler, Input.UV, 0).rgb;
+    }
+    float3 Diffuse = Lighting(Output.WorldPosition, Output.WorldNormal, ViewWorldLocation, DiffuseColor, Material.SpecularColor, Material.Shininess);
+    Output.Color = float4(Diffuse.rgb, 1.0);
+#else
+    Output.Color = Input.Color;
+#endif
+    
+    return Output;
 }
