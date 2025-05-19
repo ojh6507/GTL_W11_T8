cf9b2a43493f585af3da63612ce1e94b6ba3acfb
허승빈 <qmfhfl02@gmail.com>
2025-05-20 00:17:30 +0900

    Mesh Render 기반 마련

--- RandEngine/RandEngine/Engine/Source/Runtime/Renderer/ParticleRenderPass.cpp
+++ RandEngine/RandEngine/Engine/Source/Runtime/Renderer/ParticleRenderPass.cpp
@@ -6,6 +6,8 @@
 #include "UObject/Casts.h"
 #include "UnrealEd/EditorViewportClient.h"
 #include <UnrealClient.h>
+#include "Components/Mesh/StaticMeshRenderData.h"
+#include "Engine/Asset/StaticMeshAsset.h"
 
 FParticleRenderPass::FParticleRenderPass()
     : BufferManager(nullptr)
@@ -216,6 +218,29 @@ void FParticleRenderPass::RenderSpriteParticle(const std::shared_ptr<FEditorView
 void FParticleRenderPass::RenderMeshParticle(const std::shared_ptr<FEditorViewportClient>& Viewport, const FDynamicMeshEmitterData* MeshEmitter)
 {
     //MeshEmitter에서 VB IB 정보 가져와서 바인딩 하고 DrawIndexedInstanced();
+    ID3D11Buffer* InstanceBuffer = MeshEmitter->VertexAllocation.VertexBuffer;
+    UINT VBStride = sizeof(FStaticMeshVertex);
+    UINT Offset = 0;
+
+    FStaticMeshRenderData* RenderData = MeshEmitter->StaticMesh->GetRenderData();
+
+    FVertexInfo VertexInfo;
+    BufferManager->CreateVertexBuffer(RenderData->ObjectName, RenderData->Vertices, VertexInfo);
+
+    Graphics->DeviceContext->IASetVertexBuffers(0, 1, &VertexInfo.VertexBuffer, &VBStride, &Offset);
+
+    FIndexInfo IndexInfo;
+    BufferManager->CreateIndexBuffer(RenderData->ObjectName, RenderData->Indices, IndexInfo);
+    if (IndexInfo.IndexBuffer)
+    {
+        Graphics->DeviceContext->IASetIndexBuffer(IndexInfo.IndexBuffer, DXGI_FORMAT_R32_UINT, 0);
+    }
+    UINT Stride = MeshEmitter->GetDynamicVertexStride();
+
+    Graphics->DeviceContext->IASetVertexBuffers(1, 1, &InstanceBuffer, &Stride, &MeshEmitter->VertexAllocation.VertexOffset);
+
+    Graphics->DeviceContext->DrawIndexedInstanced(IndexInfo.NumIndices, MeshEmitter->GetSourceData()->ActiveParticleCount, 
+                                                  0, 0, 0);
 }
 
 void FParticleRenderPass::ClearRenderArr()
@@ -267,13 +292,14 @@ void FParticleRenderPass::CreateShader()
 {
     D3D11_INPUT_ELEMENT_DESC SpriteParticleLayoutDesc[] =
     {
-        { "POSITION",    0, DXGI_FORMAT_R32G32B32_FLOAT,   0, 0,                            D3D11_INPUT_PER_VERTEX_DATA, 0 },
-        { "TEXCOORD",    0, DXGI_FORMAT_R32_FLOAT,         0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // RelativeTime
-        { "TEXCOORD",    1, DXGI_FORMAT_R32G32B32_FLOAT,   0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // OldPosition
-        { "TEXCOORD",    2, DXGI_FORMAT_R32_FLOAT,         0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // ParticleId
-        { "TEXCOORD",    3, DXGI_FORMAT_R32G32_FLOAT,      0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // Size
-        { "TEXCOORD",    4, DXGI_FORMAT_R32_FLOAT,         0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // Rotation
-        { "TEXCOORD",    5, DXGI_FORMAT_R32_FLOAT,         0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // SubImageIndex
+        {"TEXCOORD",     0, DXGI_FORMAT_R32G32_FLOAT,      0, 0,                            D3D11_INPUT_PER_VERTEX_DATA, 0 }, // UV
+        { "POSITION",    0, DXGI_FORMAT_R32G32B32_FLOAT,   0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // Position
+        { "TEXCOORD",    1, DXGI_FORMAT_R32_FLOAT,         0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // RelativeTime
+        { "TEXCOORD",    2, DXGI_FORMAT_R32G32B32_FLOAT,   0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // OldPosition
+        { "TEXCOORD",    3, DXGI_FORMAT_R32_FLOAT,         0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // ParticleId
+        { "TEXCOORD",    4, DXGI_FORMAT_R32G32_FLOAT,      0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // Size
+        { "TEXCOORD",    5, DXGI_FORMAT_R32_FLOAT,         0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // Rotation
+        { "TEXCOORD",    6, DXGI_FORMAT_R32_FLOAT,         0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // SubImageIndex
         { "COLOR",       0, DXGI_FORMAT_R32G32B32A32_FLOAT,0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_VERTEX_DATA, 0 }, // Color
     };
     HRESULT hr = ShaderManager->AddVertexShaderAndInputLayout
@@ -289,7 +315,7 @@ void FParticleRenderPass::CreateShader()
     }
 
     D3D11_INPUT_ELEMENT_DESC MeshParticleLayoutDesc[] =
-    {
+    {  
         { "COLOR",     0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 0,                            D3D11_INPUT_PER_INSTANCE_DATA, 1 },
         { "TEXCOORD",  0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_INSTANCE_DATA, 1 }, // Transform[0]
         { "TEXCOORD",  1, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, D3D11_APPEND_ALIGNED_ELEMENT, D3D11_INPUT_PER_INSTANCE_DATA, 1 }, // Transform[1]
